ee5068b57c1767b3319c8cb505d6d9e1e6abc5a0
pooja <poojadhaker1410@gmail.com>
2024-04-20 00:10:30 +0530

    simple

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+// Simple Interest
+
+let principal=Number(prompt("Enter Principal Amount"));
+let rate=Number(prompt("Enter Rate of Interest 1 to 10%"));
+let time=Number(prompt("Time Period 1-12"));
+alert((principal*rate*time)/100);
+
+// Area of circle 
+
+let radius=Number(prompt("Enter Radius you will get the area"));
+alert((Math.PI*radius*radius));
+
+// Volume of cube
+
+let edge=Number(prompt("Enter Edge you will get the area of cube"));
+alert((6*edge*edge));
+
+// Surface Area of Cylinder
+
+let Radius=Number(prompt("Enter the Radius "));
+let height=Number(prompt("Enter the Height "));
+alert((2*Math.PI*Radius*height));
